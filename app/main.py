29d82162ccf52edb3118177625dd791bcde8cdd3
ernest-creiser <ernest.creiser@qantev.com>
2025-03-03 09:40:34 +0000

    add sign/ and verify/

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 import base64
 import json
-from typing import Any
+from typing import Any, Literal
 
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response
 from pydantic import BaseModel
 
 app = FastAPI()
-
-
-class Payload(BaseModel):
-    content: str
 
 
 def encode(msg: str) -> str:
@@ -28,3 +24,37 @@ def encrypt(payload: dict = Body()) -> dict[str, Any]:
 @app.post("/decrypt")
 def decrypt(payload: dict = Body()) -> dict[str, Any]:
     return {key: json.loads(decode(val)) for key, val in payload.items()}
+
+
+import os
+
+secret_key = os.getenv("SECRET_KEY", default="secret-key")
+
+import hmac
+import hashlib
+
+
+@app.post("/sign")
+def sign(payload: Any = Body()) -> dict[Literal["signature"], str]:
+    hm = hmac.new(
+        secret_key.encode("utf-8"),
+        json.dumps(payload).encode("utf-8"),
+        digestmod=hashlib.sha256,
+    )
+    return {"signature": hm.hexdigest()}
+
+
+class VerifyPayload(BaseModel):
+    signature: str
+    data: Any
+
+
+@app.post("/verify")
+def verify(payload: VerifyPayload) -> dict:
+    hm = hmac.new(
+        secret_key.encode(), json.dumps(payload.data).encode(), digestmod=hashlib.sha256
+    )
+    if payload.signature == hm.hexdigest():
+        return Response(status_code=204)
+    else:
+        return Response(status_code=422)
--- app/test_app.py
+++ app/test_app.py
@@ -7,17 +7,17 @@ from app.main import app
 client = TestClient(app)
 
 
-path_to_payload = "app/payload.json"
-
-
-@pytest.mark.parametrize("input,expected", [("app/payload.json", 200), ("app/payload-array.json", 422)])
-def test_encrypt_200(input:str, expected:int):
+@pytest.mark.parametrize(
+    "input,expected", [("app/payload.json", 200), ("app/payload-array.json", 422)]
+)
+def test_encrypt_200(input: str, expected: int):
     with open(input) as f:
         response = client.post("/encrypt", json=json.load(f))
     assert response.status_code == expected
 
+
 @pytest.mark.parametrize("input", ["app/payload.json"])
-def test_encrypt_decrypt(input:str):
+def test_encrypt_decrypt(input: str):
     with open(input) as f:
         payload = json.load(f)
 
@@ -29,3 +29,19 @@ def test_encrypt_decrypt(input:str):
     decrypted_payload = response_decrypt.json()
     assert len(decrypted_payload) == len(payload)
     assert all([v == payload[k] for k, v in decrypted_payload.items()])
+
+
+@pytest.mark.parametrize("input", ["app/payload.json", "app/payload-array.json"])
+def test_sign_and_verify(input: str):
+    with open(input) as f:
+        payload = json.load(f)
+
+    response_sign = client.post("/sign", json=payload).json()
+    response_verify = client.post(
+        "/verify",
+        json={
+            "signature": response_sign["signature"],
+            "data": payload,
+        },
+    )
+    assert response_verify.status_code == 204
